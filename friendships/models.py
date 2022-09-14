3a309529c7f81cce51ddd39004a52305a17e92b2
Yue Cao <jennyjennycomeon@gmail.com>
2022-09-14 05:00:00 +0000

    friendships and test done!

---
+++ friendships/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Friendship(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='following_friendship_set',
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='follower_friendship_set',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (
+            # obtain all that I'm following and order in time followed
+            ('from_user_id', 'created_at'),
+            # obtain all that follow me, and order in time following
+            ('to_user_id', 'created_at'),
+        )
+        unique_together = (('from_user_id', 'to_user_id'),)
+
+    def __str__(self):
+        return '{} followed {}'.format(self.from_user_id, self.to_user_id)
+
