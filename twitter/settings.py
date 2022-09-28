6e963a99b2145c053a85a1b37dfb2a51abd19d2e
Yue Cao <jennyjennycomeon@gmail.com>
2022-09-28 23:23:36 +0000

    Debug toolbar added!

--- twitter/settings.py
+++ twitter/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'notifications',
+    'debug_toolbar',
 
     # project apps
     'accounts',
@@ -69,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
@@ -144,6 +146,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 try:
     from .local_settings import *
