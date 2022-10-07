c7da02d8cf5fe3dfb90f54044bde118172572418
Yue Cao <jennyjennycomeon@gmail.com>
2022-10-07 01:11:22 +0000

    redis tweet done!

--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models.signals import post_save
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save, pre_delete
 from likes.models import Like
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
+from tweets.listeners import push_tweet_to_cache
 from utils.listeners import invalidate_object_cache
 from utils.memcached_helper import MemcachedHelper
 from utils.time_helpers import utc_now
@@ -94,3 +94,4 @@ class TweetPhoto(models.Model):
 # hook up with listeners to invalidate cache
 post_save.connect(invalidate_object_cache, sender=Tweet)
 pre_delete.connect(invalidate_object_cache, sender=Tweet)
+post_save.connect(push_tweet_to_cache, sender=Tweet)
