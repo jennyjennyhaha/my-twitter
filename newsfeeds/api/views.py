997a65d103d7fbf3e84046a062fc1ae4f165a98f
Yue Cao <jennyjennycomeon@gmail.com>
2022-10-07 05:28:16 +0000

    redis newsfeeds done!

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from newsfeeds.models import NewsFeed
 from newsfeeds.api.serializers import NewsFeedSerializer
+from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from utils.paginations import EndlessPagination
 
 
@@ -11,19 +10,16 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        # 自定义 queryset，因为 newsfeed 的查看是有权限的
-        # 只能看 user=当前登录用户的 newsfeed
-        # 也可以是 self.request.user.newsfeed_set.all()
-        # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
-        return NewsFeed.objects.filter(user=self.request.user)
-
     def list(self, request):
-        queryset = self.paginate_queryset(self.get_queryset())
+        cached_newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginator.paginate_cached_list(cached_newsfeeds, request)
+        # None: requested data not in cache, need to go to db
+        if page is None:
+            queryset = NewsFeed.objects.filter(user=request.user)
+            page = self.paginate_queryset(queryset)
         serializer = NewsFeedSerializer(
-            queryset,
+            page,
             context={'request': request},
             many=True,
         )
         return self.get_paginated_response(serializer.data)
-
--- testing/testcases.py
+++ testing/testcases.py
@@ -1,12 +1,13 @@
-from django.test import TestCase as DjangoTestCase
-from django.contrib.auth.models import User
-from tweets.models import Tweet
 from comments.models import Comment
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import caches
+from django.test import TestCase as DjangoTestCase
+from friendships.models import Friendship
 from likes.models import Like
-from rest_framework.test import APIClient
 from newsfeeds.models import NewsFeed
+from rest_framework.test import APIClient
+from tweets.models import Tweet
 from utils.redis_client import RedisClient
 
 
@@ -22,6 +23,9 @@ class TestCase(DjangoTestCase):
         # cannot be User.objects.create()
         # since password needs encryption and username and email needs some normalize
         return User.objects.create_user(username, email, password)
+
+    def create_friendship(self, from_user, to_user):
+        return Friendship.objects.create(from_user=from_user, to_user=to_user)
 
     def create_tweet(self, user, content=None):
         if content is None:
